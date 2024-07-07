df94ecf308b540abbb69f040997066ec9dd5392f
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-07 13:19:07 +0530

    Generated: `Discord-Bot-Music/src/features/timer.js`

---
+++ Discord-Bot-Music/src/features/timer.js
@@ -0,0 +1,9 @@
+const timer = {
+  startTimer: (duration) => {
+    setTimeout(() => {
+      // Logic to stop music playback after specified duration
+    }, duration);
+  },
+};
+
+module.exports = timer;
